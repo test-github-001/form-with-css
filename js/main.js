ea5f90a3f0adf545646ff774380b799eedb88887
test-github-001 <107862407+test-github-001@users.noreply.github.com>
2022-07-28 10:46:20 +0300

    upload project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/***********************
+ * 
+ *  UPDATE HIGHT VALUE
+ * 
+ */
+
+const heightValue = document.getElementById('heightValue');
+const getHeight = value => heightValue.innerText = value;
